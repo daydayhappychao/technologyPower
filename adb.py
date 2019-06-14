e8aa62a2505fae328bd1eabbda4a3f52058f3e34
Magin-CC <573746388@qq.com>
2019-06-14 16:34:37 +0800

    daily update

--- adb.py
+++ adb.py
@@ -4,44 +4,47 @@ import random
 
 i = 0
 sleep_time = 2 
-#用popen设置shell=True不会弹出cmd框
-process = subprocess.Popen('adb shell input tap 565 1713', shell=True)
+process = subprocess.Popen('adb shell input tap 405 1398', shell=True)
 time.sleep(sleep_time)
-process = subprocess.Popen('adb shell input tap 640 300', shell=True)
-time.sleep(sleep_time)
+process = subprocess.Popen('adb shell input tap 323.6 149.8', shell=True)
+time.sleep(10)
+process = subprocess.Popen('adb shell input swipe 500 1050 500 200', shell=True)
+time.sleep(10)
 
 # 看6片文章，每篇2min
 for i in range(7):
-  process = subprocess.Popen('adb shell input tap 500 ' + str(600 + 330 * (i % 3)), shell=True)
+  process = subprocess.Popen('adb shell input tap 500 ' + str(280 + 300 * (i % 3)), shell=True)
   time.sleep(sleep_time)
 
   count = 60
   if i > 1:
     count = 6
-  for j in range(60):
+  for j in range(70):
+    process = subprocess.Popen('adb shell input tap 500 850', shell=True)
+    time.sleep(1)
     process = subprocess.Popen('adb shell input swipe 500 1000 500 850', shell=True)
-    time.sleep(2)
+    time.sleep(1)
   # 收藏
-  subprocess.Popen('adb shell input tap 844 1735', shell=True)
+  subprocess.Popen('adb shell input tap 695 1403', shell=True)
   time.sleep(2)
   # 分享
-  subprocess.Popen('adb shell input tap 980 1735', shell=True)
+  subprocess.Popen('adb shell input tap 764 1400', shell=True)
   time.sleep(2)
-  subprocess.Popen('adb shell input tap 974 967', shell=True)
+  subprocess.Popen('adb shell input tap 100 1200', shell=True)
   time.sleep(2)
-  process = subprocess.Popen('adb shell input keyevent KEYCODE_BACK', shell=True)
-  time.sleep(2)
+  # process = subprocess.Popen('adb shell input keyevent KEYCODE_BACK', shell=True)
+  # time.sleep(2)
 
   # 评论
   if i < 2:
-    subprocess.Popen('adb shell input tap 384 1750', shell=True)
+    subprocess.Popen('adb shell input tap 200 1400', shell=True)
     time.sleep(5)
 
     # for i in range(40):
     #   subprocess.Popen('adb shell input keyevent 67', shell=True)
     commentKey = random.randint(1, 5)
     if commentKey == 1:
-      subprocess.Popen('adb shell am broadcast -a ADB_INPUT_TEXT --es msg "祖国万岁！"', shell=True)
+      subprocess.Popen('adb shell am broadcast -a ADB_INPUT_TEXT --es msg "中华人民共和国万岁！"', shell=True)
     elif commentKey == 2:
       subprocess.Popen('adb shell am broadcast -a ADB_INPUT_TEXT --es msg "富强、民主、文明、和谐"', shell=True)
     elif commentKey == 3:
@@ -51,7 +54,7 @@ for i in range(7):
     elif commentKey == 5:
       subprocess.Popen('adb shell am broadcast -a ADB_INPUT_TEXT --es msg "跟着共产党走，好日子还在后头"', shell=True)
     time.sleep(2)
-    subprocess.Popen('adb shell input tap 972 1525', shell=True)
+    subprocess.Popen('adb shell input tap 762 1294', shell=True)
     time.sleep(5)
 
 
@@ -64,25 +67,29 @@ for i in range(7):
 
 #  进入视频
 
-process = subprocess.Popen('adb shell input tap 755 1729', shell=True)
+process = subprocess.Popen('adb shell input tap 570 1400', shell=True)
 time.sleep(sleep_time)
-process = subprocess.Popen('adb shell input tap 600 300', shell=True)
+process = subprocess.Popen('adb shell input tap 313 140', shell=True)
 time.sleep(10)
-process = subprocess.Popen('adb shell input swipe 500 1500 500 1040', shell=True)
+process = subprocess.Popen('adb shell input swipe 500 1000 500 700', shell=True)
 time.sleep(sleep_time)
 
 # 看6个视频
 for i in range(7):
-  process = subprocess.Popen('adb shell input tap 500 ' + str(640 + 400 * (i % 3)), shell=True)
+  process = subprocess.Popen('adb shell input tap 500 ' + str(370 + 110 * (i - 1)), shell=True)
   time.sleep(sleep_time)
-  for j in range(8):
-    process = subprocess.Popen('adb shell input swipe 270 377 500 377', shell=True)
-    time.sleep(30)
+  if i == 0: 
+    time.sleep(1830)
+  else:
+    # for j in range(8):
+    #   process = subprocess.Popen('adb shell input swipe 270 377 320 377', shell=True)
+    #   time.sleep(30)
+    time.sleep(240)
   process = subprocess.Popen('adb shell input keyevent KEYCODE_BACK', shell=True)
   time.sleep(sleep_time)
-  if i == 2 or i == 4: 
-    subprocess.Popen('adb shell input swipe 500 1500 500 800', shell=True)
-    time.sleep(sleep_time)
+#  if i == 2 or i == 4: 
+#    subprocess.Popen('adb shell input swipe 500 1500 500 800', shell=True)
+#    time.sleep(sleep_time)
 
 
 
--- readme.md
+++ readme.md
@@ -5,3 +5,14 @@
 ``` python3 index.py ```
 
 个人娱乐项目请勿传播及用于商业用途！
+
+
+> 注意： 使用headlesschrome的index.py已经不能加分了, 改用安卓端的adb.py
+1. 依赖:    
+- osx(win不知道能不能用) 
+- python3
+- adb 
+- 网易mumu模拟器(需安装并使用[ADBKey](https://github.com/senzhk/ADBKeyBoard), 用于提交评论)
+2. 使用:
+- 打开xxqgAPP，登录
+- ``python3 adb.py``
